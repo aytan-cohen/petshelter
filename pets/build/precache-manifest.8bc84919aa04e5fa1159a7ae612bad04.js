37e8db06045eeb7cbcb8fa7db74a0aaef8d38fc5
Jewdeer21 <ayecohen21@gmail.com>
2019-06-28 10:36:43 -0400

    ready to deploy

---
+++ pets/build/precache-manifest.8bc84919aa04e5fa1159a7ae612bad04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7b37428f2a3b0c65f25b43cd4d10c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2908d63147cd3839e944",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "cd668fbda4581aba4f82",
+    "url": "/static/js/2.f0c738bb.chunk.js"
+  },
+  {
+    "revision": "2908d63147cd3839e944",
+    "url": "/static/js/main.72916424.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
